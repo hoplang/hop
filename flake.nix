752e551f0dab72e796592aeb09416f7e19b8c53e
Anton Lyxell <git@lyxell.dev>
2025-07-08 14:14:44 +0200

    Add flake.nix for development environment

---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+  description = "Development environment for hop-rs";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/122034aaa1c4302ce1637d54b2003e593fb0c2da";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          nativeBuildInputs = [
+            pkgs.rustc
+            pkgs.cargo
+            pkgs.rust-analyzer
+            pkgs.rustfmt
+            pkgs.just
+          ];
+        };
+      });
+}
