b2ac4e7102c5980c43e340e1217cddbdfc5a2414
Anton Lyxell <git@lyxell.dev>
2025-08-18 11:24:17 +0200

    refactor: Move DopType into unifier

--- src/dop/mod.rs
+++ src/dop/mod.rs
@@ -2,11 +2,9 @@ pub mod parser;
 pub mod runtime;
 pub mod tokenizer;
 pub mod typechecker;
-pub mod types;
 pub mod unifier;
 
 pub use parser::{parse_expr, parse_loop_header, parse_variable_name, DopExpr};
 pub use runtime::evaluate_expr;
 pub use typechecker::typecheck_dop_expression;
-pub use types::DopType;
-pub use unifier::Unifier;
+pub use unifier::{DopType, Unifier};
--- src/dop/unifier.rs
+++ src/dop/unifier.rs
@@ -1,5 +1,39 @@
-use super::DopType;
 use std::collections::{HashMap, BTreeMap};
+use std::fmt;
+
+#[derive(Debug, Clone, PartialEq)]
+pub enum DopType {
+    Object(BTreeMap<String, DopType>, u32),
+    Array(Box<DopType>),
+    Bool,
+    String,
+    Number,
+    Void,
+    TypeVar(u32),
+}
+
+impl fmt::Display for DopType {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            DopType::Object(properties, _rest) => {
+                write!(f, "{{")?;
+                for (idx, (key, value)) in properties.iter().enumerate() {
+                    if idx > 0 {
+                        write!(f, ", ")?;
+                    }
+                    write!(f, "{}: {}", key, value)?;
+                }
+                write!(f, "}}")
+            }
+            DopType::Array(inner_type) => write!(f, "{}[]", inner_type),
+            DopType::Bool => write!(f, "boolean"),
+            DopType::String => write!(f, "string"),
+            DopType::Number => write!(f, "number"),
+            DopType::Void => write!(f, "void"),
+            DopType::TypeVar(_) => write!(f, "any"),
+        }
+    }
+}
 
 #[derive(Debug, Clone, PartialEq)]
 pub struct UnificationError {
